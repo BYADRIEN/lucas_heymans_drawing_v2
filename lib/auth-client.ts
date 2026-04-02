63d2337d8dd10691e1e8fdcda2731599864fbd4e
adrien sellegit config --global user.name adrien <selleadrien@gmail.com>
2026-04-02 23:13:54 +0200

    02/04/2026

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 import { twoFactorClient } from "better-auth/plugins";
 
+// On utilise NEXT_PUBLIC_BETTER_AUTH_URL si défini, sinon localhost pour dev
 const baseURL =
   process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000";
 
