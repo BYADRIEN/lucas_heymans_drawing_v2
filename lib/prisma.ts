332c5932d8921496a4b3d58b21c54489dbadef8d
Adrien Selle <94170067+BYADRIEN@users.noreply.github.com>
2026-04-02 19:59:33 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,12 @@
 
-import { PrismaPg } from "@prisma/adapter-pg"; 
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client"; // <--- CHANGE CETTE LIGNE
+
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-export default prisma; 
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
